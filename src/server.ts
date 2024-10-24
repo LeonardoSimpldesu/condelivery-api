065ebad8e21dd1effb1d7d50a168fc6e9185c4de
Leonardo <leonardodesouzaalmeidas@gmail.com>
2024-10-24 20:33:05 -0300

    ao aguardo de um milagre

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import { app } from "./app"
 import { env } from "./env";
 
-app
+
+if (process.env.NODE_ENV !== 'production') {
+  app
   .listen({
     host: "0.0.0.0",
     port: env.PORT,
@@ -9,3 +11,9 @@ app
   .then(() => {
     console.log("✅ HTTP Server Running!");
   });
+}
+
+  export default async function handler(req: any, res: any) {
+    await app.ready()
+    app.server.emit('request', req, res)
+  }
